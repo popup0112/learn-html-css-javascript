d87e8493a5540b54d44cc7e32eefd1617db824a4
popup0112 <popup0112@gmail.com>
2024-03-16 21:36:32 +0900

    FEAT:css 활용 화면 꾸미기(text-align,border,transition,hover)

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const input = document.querySelector("#입력창");
 const todos = document.querySelector("#할일들");
 button.addEventListener("click", () => {
   const value = input.value;
+
   const p = document.createElement("p");
   p.innerText = value;
+
   todos.appendChild(p);
+  input.value = "";
+
+  p.addEventListener("click", () => {
+    if (p.className !== "done") {
+      p.className = "done";
+    } else {
+      p.className = "";
+    }
+  });
 });
